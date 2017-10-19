62c01a0417d77b20f20ea658feff580954bf7512
Zanzes <nikolajea@gmail.com>
2017-10-19 11:11:01 +0200

    All ratings complete

--- Java/Work/src/main/java/App.java
+++ Java/Work/src/main/java/App.java
@@ -1,10 +1,34 @@
 import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class App 
 {
     public static void main(String[] args) throws InterruptedException, IOException
     {
-    	Analyzer.main(new String[] {"reviews.txt"});
+    	TreeMap<String, PriorityQueue<Integer>> map= new TreeMap<>();
+    	PriorityQueue<Integer> queueA = new PriorityQueue<>();
+    	PriorityQueue<Integer> queueB = new PriorityQueue<>();
+    	queueA.add(1);
+    	queueA.add(2);
+    	queueB.add(1);
+    	queueB.add(2);
+    	map.put("First", queueA);
+    	map.put("Second", queueB);
+    	
+    	TreeMap<String, Integer> ret = MovieRatingsProcessor.removeAllRatingsBelow(map, 3);
+    	Set<Entry<String, Integer>> entrySet = ret.entrySet();
+    	if (entrySet.size() <= 0)
+		{
+    		System.out.println("NONE!");			
+		}
+    	for (Entry<String, Integer> entry : entrySet)
+		{
+			System.out.println(entry.getKey()+" : "+entry.getValue());
+		}
+    	
     }
     
     static void br()
--- Java/Work/src/main/java/MovieRatingsProcessor.java
+++ Java/Work/src/main/java/MovieRatingsProcessor.java
@@ -31,25 +31,35 @@ public class MovieRatingsProcessor
 	public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings, int rating)
 	{
 		TreeMap<String, Integer> map = new TreeMap<>();
+		ArrayList<String> remove = new ArrayList<>();
 		try
 		{
 			Set<java.util.Map.Entry<String, PriorityQueue<Integer>>> entrySet = movieRatings.entrySet();
 			for (java.util.Map.Entry<String, PriorityQueue<Integer>> entry : entrySet)
-			{
 				if (entry.getValue().peek() < rating)
 				{
 					int i = 0;
-					while (entry.getValue().peek() < rating)
-					{
-						entry.getValue().remove();
-						i++;
-					}
-						map.put(entry.getKey(), i);
+					int size = entry.getValue().size();
+					for (int j = 0; j < size; j++)
+						if (entry.getValue().peek() < rating)
+						{
+							entry.getValue().remove();
+							i++;
+						}
+						else
+							break;
+					if (i == size)
+						remove.add(entry.getKey());
+					
+					map.put(entry.getKey(), i);
 				}
-			}
 		}
 		catch (Exception e)
 		{}
+		for (String string : remove)
+		{
+			movieRatings.remove(string);
+		}
 		return map;
 	}
 }
